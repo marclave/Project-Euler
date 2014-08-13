ae465d64bc1ed20efe1455a26222f25a4de4178e
Marc Laventure <marcman@shaw.ca>
2014-08-12 21:42:38 -0700

    Initial commit of working Problem16.py

---
+++ Problem16.py
@@ -0,0 +1,33 @@
+"""
+Problem 16
+https://projecteuler.net/problem=16
+Power digit sum
+"""
+
+def powerSum(power):
+
+	sum = 0
+	result = str(2**power)
+
+	for i in range(len(result)):
+		sum += int(result[i])
+
+	print sum
+
+
+
+
+if __name__ == "__main__":
+
+	print "================================="
+	print "            Problem16            "
+	print "    Developed by Marc Laventure  "
+	print "================================="
+	print ""
+
+	powerSum(1000)
+
+
+	
+
+	
